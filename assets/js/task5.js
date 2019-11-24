dab999d815a8e796578eb1628ca178d8a3feed79
AndriiTaran <tarangad313@gmail.com>
2019-11-24 23:27:54 +0200

    'upgrade2'

--- assets/js/task5.js
+++ assets/js/task5.js
@@ -1,1 +1,27 @@
 "use strict";
+
+function whoLonger() {
+
+    let n1 = prompt('Enter your line 1');
+    let n2 = prompt('Enter your line 2');
+    if (n1.length > n2.length){
+
+        return 1;
+
+    }
+    else if (n1.length<n2.length){
+
+        return -1;
+
+    }
+    return 0;
+}
+
+function findLetter() {
+    let str = ["e", "y", "u", "i", "o", "a"];
+    let n = prompt("Enter your line");
+
+    let k = [...n.toLowerCase()].reduce((a, b) => a + str.includes(b), 0);
+
+    return k
+}
